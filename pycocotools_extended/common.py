647fbfd6f2c226b3d57e4bd7504c4eb34009b404
Roman Vey <roman.vey@gmail.com>
2019-10-07 12:01:46 +0300

    fixes

--- pycocotools_extended/common.py
+++ pycocotools_extended/common.py
@@ -228,8 +228,9 @@ def clean(anns_path, save_path):
     new_img_ids = set()
 
     for image in data['images']:
+        print(image)
         try:
-            _ = _read_img(image['filename'])
+            _ = _read_img(image['file_name'])
             new_imgs.append(image)
             new_img_ids.add(image['id'])
         except ValueError:
